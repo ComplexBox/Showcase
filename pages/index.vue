eaf1501a55388f031581b465e9e7dec97b3acc9f
Il Harper <afanyiyu@hotmail.com>
2020-08-29 22:26:08 +0800

    feat: Add Transitions

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="container">
-    <div v-if="loading" class="flex-center flex-column">
-      <div class="flex-center">
-        <img src="@/assets/splash.png" style="width: 200px" />
+    <transition appear name="fade" mode="out-in">
+      <div v-if="loading" key="loading" class="flex-center flex-column">
+        <div class="flex-center">
+          <img src="@/assets/splash.png" style="width: 200px" />
+        </div>
+        <p style="color: white">
+          未能连接到主播端，请检查SimpleBox是否已经启动。
+        </p>
       </div>
-      <p style="color: white">
-        未能连接到主播端，请检查SimpleBox是否已经启动。
-      </p>
-    </div>
-    <div v-else class="flex-center flex-column">
-      <div class="question-box">
-        <div class="text-field">{{ data.msg }}</div>
-        <div class="extra-field"></div>
+      <div v-else key="loaded" class="flex-center flex-column">
+        <div class="question-box">
+          <div class="text-field">
+            <transition appear name="fade" mode="out-in">
+              <div>
+                {{ data.msg }}
+              </div>
+            </transition>
+          </div>
+
+          <div class="extra-field"></div>
+        </div>
+        <div v-if="data.response" class="answer-box">
+          <div class="text-field">
+            <transition appear name="fade" mode="out-in">
+              <div>
+                {{ data.response }}
+              </div>
+            </transition>
+          </div>
+
+          <div class="extra-field"></div>
+        </div>
       </div>
-      <div v-if="data.response" class="answer-box">
-        <div class="text-field">{{ data.response }}</div>
-        <div class="extra-field"></div>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
