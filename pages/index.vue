d7375a70bdfd828b0aa6c44898ba68628bb66f37
Il Harper <afanyiyu@hotmail.com>
2020-08-29 18:40:34 +0800

    feat: Index Layout Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <p>{{ msg }}</p>
-    <p>{{ response }}</p>
+  <div class="container">
+    <div v-if="loading" class="flex-center flex-column">
+      <div class="flex-center">
+        <img src="@/assets/splash.png" style="width: 200px" />
+      </div>
+      <p>未能连接到主播端，请检查SimpleBox是否已经启动。</p>
+    </div>
+    <div v-else>
+      <p>{{ msg }}</p>
+      <p>{{ response }}</p>
+    </div>
   </div>
 </template>
 
